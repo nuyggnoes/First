4cb4523bcca664924a8239bdbf3e9e403fbf2eec
nuyggnoes <tjdrbs0607@naver.com>
2022-01-08 20:33:25 +0900

    3052번

---
+++ BAEK_배열/나머지.py
@@ -0,0 +1,10 @@
+a=[]
+count=1
+for i in range(10):
+    n = int(input())  # int형으로 n에 저장
+    n %= 42  # 42로 나눈 나머지를 다시 n에 저장 후
+    a.append(n)  # 리스트 a에 바로 추가
+
+for i in range(len(a)):
+    if a.count(i) > 1 : a.remove(i)
+print(a)
