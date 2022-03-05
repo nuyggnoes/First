5968ff7bd0dcd3b23ccf4cf70c5fb851315974f1
nuyggnoes <tjdrbs0607@naver.com>
2022-03-06 00:51:54 +0900

    time

---
+++ BAEK_기본수학1/prac.py
@@ -0,0 +1,5 @@
+a = 4
+print(type(a))
+n = [-1]*a
+print(n)
+print(*n)
--- BAEK_스택/17298(시간초과).py
+++ BAEK_스택/17298(시간초과).py
@@ -1,16 +1,30 @@
 import sys
-n = int(sys.stdin.readline())
-a = list(map(int,sys.stdin.readline().split()))
-stack = []
+from collections import deque
 
-for i in a:
-    for j in a:
-        if i < j:
-            stack.append(j)
-            a = a[1:]
-            break
-        elif j == a[len(a)-1]:
-            stack.append(-1)
-            a = a[1:]
-for i in stack:
-    print(i, end = ' ')
+n = int(sys.stdin.readline())
+a = list(map(int,sys.stdin.readline().rstrip().split()))
+stack = []
+ans = []
+
+for i in range(n - 1):
+    x = i
+    stack.append(a[i])
+    if stack[-1] < a[i+1]:
+        ans.append(a[i+1])
+        stack.pop()
+    else:
+        if (i+2 == n):
+            if stack[-1] < a[i+1]:
+                ans.append(a[i+1])
+                break
+            else:
+                ans.append(-1)
+        else:
+            for x in range(i+2,n):
+                if stack[-1] < a[x]:
+                    ans.append(a[x])
+                    break
+                elif x == n-1:
+                    ans.append(-1)
+ans.append(-1)
+print(*ans)
