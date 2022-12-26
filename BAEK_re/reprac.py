a1d2a29c0d0eb9b1282a61e5eaea3f94325318bc
nuyggnoes <tjdrbs0607@naver.com>
2022-12-26 23:00:54 +0900

    re_py

--- BAEK_re/reprac.py
+++ BAEK_re/reprac.py
@@ -1,14 +1,14 @@
 import sys
 input = sys.stdin.readline
-N = int(input())
-n = list(map(int,input().split()))
-n.sort()
-M = int(input())
-m = list(map(int,input().split()))
-m.sort()
-print(n,m)
-# for i in m:
-#     if i in n:
-#         print(1)
-#     else:
-#         print(0)
+t = int(input())
+for i in range(t):
+    sum = 0
+    score = 1
+    quiz_result = input().rstrip()
+    for i in quiz_result:
+        if i == 'O':
+            sum += score
+            score += 1
+        elif i == 'X':
+            score = 1
+    print(sum)
