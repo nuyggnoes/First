46d14d2e918baf4f4084149204843780170d7130
nuyggnoes <tjdrbs0607@naver.com>
2023-03-25 22:39:28 +0900

    이분탐색

--- BAEK_정렬/10815.py
+++ BAEK_정렬/10815.py
@@ -1,16 +1,21 @@
 import sys
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
-cards = list(map(int, sys.stdin.readline().split()))
-M = int(sys.stdin.readline())
-checks = list(map(int, sys.stdin.readline().split()))
+N = int(input())
+cards = sorted(list(map(int,input().split())))
+M = int(input())
+checks = list(map(int,input().split()))
 
-dict = {}
-for i in range(len(cards)):
-    dict[cards[i]] = 0 
-
-for j in range(M):
-    if checks[j] not in dict:
-        print(0, end=' ')
-    else:
-        print(1, end=' ')
+for check in checks:
+    low, high = 0, N-1
+    flag = False
+    while low<=high:
+        mid = (low+high)//2
+        if cards[mid] > check:
+            high = mid - 1
+        elif cards[mid] < check:
+            low = mid + 1
+        else:
+            flag = True
+            break
+    print(1 if flag else 0, end = ' ')
