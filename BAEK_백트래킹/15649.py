766a62ca6f8e78833ae241958e6cfa8b85103c05
nuyggnoes <tjdrbs0607@naver.com>
2023-03-25 23:51:26 +0900

    백트래킹 입문

---
+++ BAEK_백트래킹/15649.py
@@ -0,0 +1,14 @@
+N,M = map(int,input().split())
+S = []
+
+def dfs():
+    if len(S) == M:
+        print(' '.join(map(str,S)))
+        return
+    for i in range(1, N+1):
+        if i not in S:
+            S.append(i)
+            dfs()
+            S.pop()
+
+dfs()
