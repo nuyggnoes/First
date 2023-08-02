fd5926e9144e2ad4199687f96996062a35ca95ce
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2023-08-02 20:50:32 +0900

    Added page count function

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,7 +1,54 @@
-from requests import get
 from bs4 import BeautifulSoup
-
 from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-jobs = extract_wwr_jobs("python")
-print(jobs)
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+browser = webdriver.Chrome(options=options)
+
+def get_page_count(keyword):
+    base_url = "https://kr.indeed.com/jobs?q="
+    browser.get(f"{base_url}{keyword}")
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    pagination = soup.find("nav", class_= "ecydgvn0")
+    if pagination == None:
+        return 1
+    pages = pagination.find_all("div",recursive=False)
+    count = (len(pages))
+    if count >= 5:
+        return 5
+    else:
+        return count
+
+
+def extract_indeed_jobs(keyword):
+    pages = get_page_count(keyword)
+    for page in range(pages):
+        base_url = "https://kr.indeed.com/jobs"
+        browser.get(f"{base_url}?q={keyword}&start={page*10}")
+
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        job_list = soup.find("ul", class_="jobsearch-ResultsList")
+        jobs = job_list.find_all("li", recursive=False)
+
+        results = []
+        for job in jobs:
+            zone = job.find("div", class_= "mosaic-zone")
+            if zone == None:
+                anchor = job.select_one("h2 a")
+                title = anchor['aria-label']
+                link = anchor['href']
+                company = job.find("span", class_= "companyName")
+                location = job.find("div", class_= "companyLocation")
+                job_data = {
+                    'link' : f"https://kr.indeed.com{link}",
+                    'company' : company.string,
+                    'location' : location.string,
+                    'position' : title
+                }
+                results.append(job_data)
+        for result in results:
+            print(result)
+            print("\n================================\n")
