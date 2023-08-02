428953861e024579064df077b56781b5bbd5c8ee
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2023-08-02 15:58:30 +0900

    beautiful soup!

--- web_scrapping.py
+++ web_scrapping.py
@@ -11,4 +11,9 @@ if response.status_code != 200:
     print("Can't request website")
 else:
     soup = BeautifulSoup(response.text, "html.parser")
-    print(soup.find_all('section', class_="jobs"))
+    jobs = soup.find_all('section', class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        for post in job_posts:
+            print(post)
+            print("//////////////////////////////")
