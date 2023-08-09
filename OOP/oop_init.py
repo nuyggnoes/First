c6eec984daee142ef79f35630a0ff3c812608c93
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2023-08-09 23:40:06 +0900

    OOP(2)

--- OOP/oop_init.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    def __init__(self, name, xp):
-        self.name = name
-        self.xp = xp
-    def say_hello(self):
-        print(f"hello my name is {self.name}")
-
-steve = Player("steve", 1000)
-steve.say_hello()
---
+++ OOP/oop_prac.py
@@ -0,0 +1,20 @@
+class Human:
+    def __init__(self,name):
+        self.name=name 
+
+    def say_hello(self):
+        print(f"hello my name is {self.name}")
+
+class Player(Human):
+    def __init__(self, name, xp):
+        super().__init__(name)
+        self.xp = xp
+
+class Fan(Human):
+    def __init__(self,name,fav_team):
+        super().__init__(name)
+        self.fav_team=fav_team
+
+steve_player = Player("steve", 1000)
+steve_fan = Fan("steve_fan", "dontknow")
+steve_player.say_hello()
